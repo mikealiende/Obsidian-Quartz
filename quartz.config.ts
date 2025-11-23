77726419b9625c1ce84d9473cb875616621ca5ad
Miguel Aliende <m_aliende98@hotmail.com>
2025-11-23 18:18:47 +0100

    Configure GitHub Pages deployment

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "es-ES",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "mikealiende.github.io/Obsidian-Quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
